9f4ece381b9968870134c599423c4d28d73ce3f2
NastyaAndreeva <andreeva.anastasiya.v@gmail.com>
2023-07-09 15:31:19 +0300

    updated task1

--- first_class_functions/task1.py
+++ first_class_functions/task1.py
@@ -1,7 +1,5 @@
 def define_number_of_local_vars(a, b):
     count = 0
-    for _ in locals():
-        count += 1
-    return count
+    return len(locals())
 
 print(define_number_of_local_vars(3, 4))
