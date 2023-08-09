4358be4dfff7ed91aa45bb7a46afa52e58fda715
NastyaAndreeva <andreeva.anastasiya.v@gmail.com>
2023-08-09 10:25:18 +0300

    added tests hw 2

--- tests/dictionary.py
+++ tests/dictionary.py
@@ -15,23 +15,27 @@ def writer_wrapper():
         raise FileNotFoundError
 
 
-if __name__ == "__main__":
+def run_dictionary():
     operation = input("Please, type the operation that you want to implement: add (a), update (u), search (s), delete (d), or exit (e): ")
 
     if (operation.lower() == "a" or operation.lower() == "add"):
         add_entry(data, input("Please, enter first name: "), input("Please, enter last name: "), input("Please, enter your city: "), input("Please, enter your phone: "))
         writer_wrapper()
+        return "Success"
     elif (operation.lower() == "u" or operation.lower() == "update"):
         update_entry(data,"+6666666668", "city", "Munchen")
         writer_wrapper()
+        return "Success"
     elif (operation.lower() == "s" or operation.lower() == "search"):
         print(search_by_field(data, "city", "Tokyo"))
         print(search_by_field(data, "phone", "+6666666668"))
         print(search_by_name(data, "Kaiser Michael"))
+        return "Success"
     elif (operation.lower() == "d" or operation.lower() == "delete"):
         delete_entry(data, "+6666666666")
         writer_wrapper()
+        return "Success"
     elif (operation.lower() == "e" or operation.lower() == "exit"):
-        pass
+        return
     else:
-        print("The operation is invalid")
+       return "The operation is invalid"
